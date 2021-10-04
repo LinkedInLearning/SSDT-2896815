4a2d2607f4aaf01be66da5fdea99cf7a3f126301
walt-instructor <staffauthor@outlook.com>
2021-10-04 12:01:21 -0700

    Add primary key to Vendors table

--- source/SSDT/Northwind-Import/dbo/Tables/Vendors.sql
+++ source/SSDT/Northwind-Import/dbo/Tables/Vendors.sql
@@ -1,5 +1,7 @@
 ﻿CREATE TABLE [dbo].[Vendors] (
     [VendorName] VARCHAR (110) NULL,
-    [TaxId]      VARCHAR (20)  NULL
+    [TaxId]      VARCHAR (20)  NULL, 
+    [VendorId] INT NOT NULL, 
+    CONSTRAINT [PK_Vendors] PRIMARY KEY ([VendorId])
 );
 
